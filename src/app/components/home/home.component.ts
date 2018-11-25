213502386e5483ee6d42b0208f4ecaa92cc684c7
woonandez <matthew.allen.fernandez@gmail.com>
2018-11-25 09:17:47 -0800

    dynamically render photos on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { 2018_Club_Championships } from 'assets/img/shmc_pics/shmc_photos';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +6,103 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  // Championship_2018: any = '/assets/img/shmc_pics/shmc_photos.js';
-
   constructor() {}
 
+  Photos = [
+    {
+      name: 'Rob Jacobus',
+      src: 'assets/img/shmc_pics/2018_club_championships/rob_jacobus.png',
+      desc: '2018 Club Champion.'
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/1.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/2.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/3.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/4.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/5.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/6.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/7.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/8.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/9.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/10.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/11.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/12.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/13.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/14.png',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2018_member_guest/15.png',
+      desc: ''
+    },
+    {
+      name: 'Brodie Munroe and Rick Cromwell',
+      src: 'assets/img/shmc_pics/2018_member_member/brodie_rick.png',
+      desc: '1st Place Gross Champions.'
+    },
+    {
+      name: 'Ron Fernandez',
+      src: 'assets/img/shmc_pics/2018_member_member/ron_fernandez.png',
+      desc: 'Shot of the Tournament - Ron Fernandez\'s tee shot on #17.  Yes he made the putt!!'
+    }
+  ]
+
+
   ngOnInit() {
-    // this.Championship_2018 = 2018_Club_Championships;
-
-    // console.log(this.Championship_2018, 'test');
   }
-
 }
+

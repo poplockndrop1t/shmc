10b6de8074af57215a6fb19798b8ebba49bf1cd5
woonandez <matthew.allen.fernandez@gmail.com>
2019-01-27 15:28:53 -0800

    adjust markup on dashboard

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -11,11 +11,11 @@ export const ROUTES: RouteInfo[] = [
   { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
   { path: '/super-report', title: 'Super Indendent Report', icon: 'notes', class: ''},
   { path: '/tournament-schedule', title: 'Tournament Schedule', icon: 'content_paste', class: '' },
-  { path: '/upcoming-event', title: 'Upcoming Event', icon: 'content_paste', class: '' },
+  { path: '/upcoming-event', title: 'Upcoming Event', icon: 'queue', class: '' },
   { path: '/handicaps', title: 'Handicaps', icon: 'poll', class: '' },
-  { path: '/new-rules', title: 'New Rules', icon: 'poll', class: '' },
-  { path: '/tournament-results', title: 'Tournament Results', icon: 'library_books', class: '' },
-  { path: '/photos', title: 'Photos', icon: 'dashboard', class: ''}
+  { path: '/new-rules', title: 'New Rules', icon: 'new_releases', class: '' },
+  { path: '/photos', title: 'Photos', icon: 'dashboard', class: ''},
+  { path: '/tournament-results', title: 'Tournament Results', icon: 'library_books', class: '' }
   // { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
 ];
 

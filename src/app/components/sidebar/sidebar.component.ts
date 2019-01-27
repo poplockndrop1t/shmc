6db25bb6cef0549badeed5925b5dfa3247520b4e
woonandez <matthew.allen.fernandez@gmail.com>
2019-01-27 15:45:32 -0800

    update sidebar icons

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -14,9 +14,9 @@ export const ROUTES: RouteInfo[] = [
   { path: '/upcoming-event', title: 'Upcoming Event', icon: 'queue', class: '' },
   { path: '/handicaps', title: 'Handicaps', icon: 'poll', class: '' },
   { path: '/new-rules', title: 'New Rules', icon: 'new_releases', class: '' },
-  { path: '/photos', title: 'Photos', icon: 'dashboard', class: ''},
+  { path: '/photos', title: 'Photos', icon: 'phone_iphone', class: ''},
   { path: '/minutes', title: 'Meeting Minutes', icon: 'dashboard', class: '' },
-  { path: '/tournament-results', title: 'Tournament Results', icon: 'library_books', class: '' }
+  { path: '/tournament-results', title: 'Tournament Results', icon: 'priority_high', class: '' }
   // { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
 ];
 

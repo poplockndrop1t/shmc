6592dcc23f579a35c7c1d5b9958eb33295d8641c
woonandez <matthew.allen.fernandez@gmail.com>
2019-01-27 14:57:43 -0800

    test pdf should populate length of page

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,9 +10,9 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
   { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
   { path: '/super-report', title: 'Super Indendent Report', icon: 'notes', class: ''},
-  { path: '/tournament-results', title: 'Tournament Results', icon: 'library_books', class: '' },
   { path: '/tournament-schedule', title: 'Tournament Schedule', icon: 'content_paste', class: '' },
   { path: '/handicaps', title: 'Handicaps', icon: 'poll', class: '' },
+  { path: '/tournament-results', title: 'Tournament Results', icon: 'library_books', class: '' },
   { path: '/photos', title: 'Photos', icon: 'dashboard', class: ''}
   // { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
 ];

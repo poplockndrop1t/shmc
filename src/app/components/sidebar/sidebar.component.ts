c478cf198d7b18dab1ba968891862ef4104d9ad0
woonandez <matthew.allen.fernandez@gmail.com>
2019-04-29 18:23:45 -0700

    update handicaps to currenkt handicaps

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -11,7 +11,7 @@ export const ROUTES: RouteInfo[] = [
   { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
   { path: '/super-report', title: 'Superindendent Report', icon: 'notes', class: ''},
   { path: '/tournament-schedule', title: 'Tournament Schedule', icon: 'content_paste', class: '' },
-  { path: '/handicaps', title: 'Handicaps', icon: 'poll', class: '' },
+  { path: '/handicaps', title: 'Current Handicaps', icon: 'poll', class: '' },
   { path: '/new-rules', title: 'New Rules', icon: 'new_releases', class: '' },
   { path: '/photos', title: 'Photos', icon: 'phone_iphone', class: ''},
   { path: '/minutes', title: 'Meeting Minutes', icon: 'mms', class: '' },

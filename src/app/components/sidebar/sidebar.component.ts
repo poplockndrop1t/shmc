07c9fbaa540a6c02725ecb341506d3c09261974f
woonandez <matthew.allen.fernandez@gmail.com>
2019-05-07 17:32:56 -0700

    Update the order of the links on the sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,13 +9,13 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
   { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-  { path: '/super-report', title: 'Superindendent Report', icon: 'notes', class: ''},
   { path: '/tournament-schedule', title: 'Tournament Schedule', icon: 'content_paste', class: '' },
-  { path: '/handicaps', title: 'Current Handicaps', icon: 'poll', class: '' },
-  { path: '/new-rules', title: 'New Rules', icon: 'new_releases', class: '' },
-  { path: '/photos', title: 'Photos', icon: 'phone_iphone', class: ''},
-  { path: '/minutes', title: 'Meeting Minutes', icon: 'mms', class: '' },
   { path: '/tournament-results', title: 'Tournament Results', icon: 'priority_high', class: '' }
+  { path: '/photos', title: 'Photos', icon: 'phone_iphone', class: ''},
+  { path: '/handicaps', title: 'Current Handicaps', icon: 'poll', class: '' },
+  { path: '/minutes', title: 'Meeting Minutes', icon: 'mms', class: '' },
+  { path: '/super-report', title: 'Superindendent Report', icon: 'notes', class: ''},
+  { path: '/new-rules', title: 'New Rules', icon: 'new_releases', class: '' }
   // { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
 ];
 

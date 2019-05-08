d078735ca589207e925951c4bab9651e121084fc
woonandez <matthew.allen.fernandez@gmail.com>
2019-05-07 18:42:54 -0700

    Code Cleanup

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,15 +8,15 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-  { path: '/dashboard',           title: 'Dashboard',             icon: 'dashboard',     class: '' },
-  { path: '/upcoming-tournament', title: 'Upcoming Tournament',   icon: 'touch_app',     class: '' },
+  { path: '/dashboard',           title: 'Dashboard',             icon: 'dashboard',      class: '' },
+  { path: '/upcoming-tournament', title: 'Upcoming Tournament',   icon: 'touch_app',      class: '' },
   { path: '/tournament-schedule', title: 'Tournament Schedule',   icon: 'calendar_today', class: '' },
-  { path: '/tournament-results',  title: 'Tournament Results',    icon: 'poll', class: '' },
-  { path: '/photos',              title: 'Photos',                icon: 'phone_iphone',  class: '' },
-  { path: '/handicaps',           title: 'Current Handicaps',     icon: 'golf_course',          class: '' },
-  { path: '/minutes',             title: 'Meeting Minutes',       icon: 'attachment',           class: '' },
-  { path: '/super-report',        title: 'Superindendent Report', icon: 'work',         class: '' },
-  { path: '/new-rules',           title: 'New Rules',             icon: 'new_releases',  class: '' }
+  { path: '/tournament-results',  title: 'Tournament Results',    icon: 'poll',           class: '' },
+  { path: '/photos',              title: 'Photos',                icon: 'phone_iphone',   class: '' },
+  { path: '/handicaps',           title: 'Current Handicaps',     icon: 'golf_course',    class: '' },
+  { path: '/minutes',             title: 'Meeting Minutes',       icon: 'attachment',     class: '' },
+  { path: '/super-report',        title: 'Superindendent Report', icon: 'work',           class: '' },
+  { path: '/new-rules',           title: 'New Rules',             icon: 'new_releases',   class: '' }
 ];
 
 @Component({

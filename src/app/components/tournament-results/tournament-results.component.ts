2ae322d419ca74d06ac28a53b829ae0ef4dba963
woonandez <matthew.allen.fernandez@gmail.com>
2019-05-11 16:30:11 -0700

    Add sparky anderson tournamnet to tournament results

--- src/app/components/tournament-results/tournament-results.component.ts
+++ src/app/components/tournament-results/tournament-results.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tournament-results.component.css']
 })
 export class TournamentResultsComponent implements OnInit {
+  sparkyAnderson2019 = [
+    {
+      flight: "A",
+      player: "Scott Linklater, and Kevin O'Toole"
+      gross: 72,
+      net: 64,
+      place: ""
+    },
+    {
+      flight: "A",
+      player: "Jeff Halloran, and Eddie Lindgren",
+      gross: 73,
+      net: 64,
+      place: ""
+    }
+  ];
+
   constructor() {}
 
   ngOnInit() {}

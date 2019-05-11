9e8b5ebe356f01b062a4384038fb967f4ec42753
woonandez <matthew.allen.fernandez@gmail.com>
2019-05-11 15:45:17 -0700

    abstract out the first row of the dashboard comp

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,6 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  firstRow = [
+    {
+      headerType: 'card-header-danger',
+      route: '/upcoming-tournament',
+      materialIcon: 'touch_app',
+      info: 'Upcoming Tournament'
+    },
+    {
+      headerType: 'card-header-success',
+      route: '/tournament-schedule',
+      materialIcon: 'calendar_today',
+      info: 'Tournament Schedule'
+    },
+    {
+      headerType: 'card-header-info',
+      route: '/tournament-results',
+      materialIcon: 'poll',
+      info: 'Tournament Results'
+    },
+    {
+      headerType: 'card-header-warning',
+      route: '/photos',
+      materialIcon: 'phone_iphone',
+      info: 'Photos'
+    }
+  ];
 
   constructor() {}
   ngOnInit() {}

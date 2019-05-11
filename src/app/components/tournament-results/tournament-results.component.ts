47134e50f8cf5f0ac838f4c4faab35d6ce22f03e
woonandez <matthew.allen.fernandez@gmail.com>
2019-05-11 16:45:40 -0700

    Add sparkyAnderson2019 tournamnet results

--- src/app/components/tournament-results/tournament-results.component.ts
+++ src/app/components/tournament-results/tournament-results.component.ts
@@ -132,11 +132,136 @@ export class TournamentResultsComponent implements OnInit {
       gross: 77,
       net: 62,
       place: ""
+    },
+    {
+      flight: "B",
+      player: "Gary Ott, and Brian Graver",
+      gross: 78,
+      net: 64,
+      place: ""
+    },
+    {
+      flight: "B",
+      player: "James Huckle, and Matt Heaney",
+      gross: 76,
+      net: 61,
+      place: "1st Net"
+    },
+    {
+      flight: "B",
+      player: "Alex Lopez, and Tim Neufeld",
+      gross: 80,
+      net: 67,
+      place: ""
+    },
+    {
+      flight: "B",
+      player: "Arold Armedilla, and David Blake",
+      gross: 76,
+      net: 61,
+      place: "3rd Net"
+    },
+    {
+      flight: "B",
+      player: "Fred Robertson, and Dave Dawson",
+      gross: 78,
+      net: 66,
+      place: ""
+    },
+    {
+      flight: "B",
+      player: "Willie Lam, and Gary Nunes",
+      gross: 79,
+      net: 63,
+      place: "5th Net"
+    },
+    {
+      flight: "B",
+      player: "John Schiffner, and Pat Chavez",
+      gross: 82,
+      net: 64,
+      place: ""
+    },
+    {
+      flight: "B",
+      player: "Greg Struck, and Scott Berger",
+      gross: 82,
+      net: 61,
+      place: "2nd Net"
+    },
+    {
+      flight: "B",
+      player: "James Meyring, and Nathan Morris",
+      gross: 89,
+      net: 70,
+      place: ""
+    },
+    {
+      flight: "B",
+      player: "Frank Sherman, and Bryan Bradley",
+      gross: 82,
+      net: 65,
+      place: ""
+    },
+    {
+      flight: "B",
+      player: "Mark Empey, and William Odell",
+      gross: 84,
+      net: 67,
+      place: ""
+    },
+    {
+      flight: "B",
+      player: "Brent Lacho, and Mark Violin",
+      gross: 75,
+      net: 58,
+      place: "1st Gross"
+    },
+    {
+      flight: "B",
+      player: "Mitch Knudson, and Dave Pearson",
+      gross: 86,
+      net: 67,
+      place: ""
+    },
+    {
+      flight: "B",
+      player: "Larry Ferris, and Chuck Zelden",
+      gross: 88,
+      net: 66,
+      place: ""
+    },
+    {
+      flight: "B",
+      player: "Chuck Kelly, and Adolf Cheung",
+      gross: 88,
+      net: 64,
+      place: ""
+    },
+    {
+      flight: "B",
+      player: "Danny Holmes, and Bob Ogg",
+      gross: 87,
+      net: 62,
+      place: "4th Net"
+    },
+    {
+      flight: "B",
+      player: "Benny McNulty, and Kenny Paschal",
+      gross: 97,
+      net: 68,
+      place: ""
+    },
+    {
+      flight: "B",
+      player: "Steve Smith, and Jon Barker",
+      gross: 96,
+      net: 67,
+      place: ""
     }
   ];
 
   constructor() {}
-
   ngOnInit() {}
 
 }

ffea7b311956fe7ca1534d58b5c3a0e902978703
woonandez <matthew.allen.fernandez@gmail.com>
2019-07-09 13:44:35 -0700

    Meeting minutes section can now be toggled

--- src/app/components/minutes/minutes.component.ts
+++ src/app/components/minutes/minutes.component.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./minutes.component.css']
 })
 export class MinutesComponent implements OnInit {
-
   constructor() {}
   ngOnInit() {}
+  toggleShown = {
+    'May 14th, 2019': true,
+    'March 12, 2019': false,
+    'January 8th, 2019': false
+  }
 
+  handleClick(e) {
+    if (this.toggleShown[e.target.innerText]) {
+      this.toggleShown[e.target.innerText] = false;
+    } else {
+      this.toggleShown[e.target.innerText] = true;
+    }
+  }
 }

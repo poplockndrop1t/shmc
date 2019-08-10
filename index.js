01bdc81b61ddad216b4b3e505be9335cf0c4f8a9
Matt <Matt@Homes-MacBook-Air.local>
2019-08-10 07:04:18 -0700

    Add boilerplate code for firebase db

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fbAdmin = require('firebase-admin');
+
+let serviceAccount = require('some path to be added');
 
 const app = express();
 const port = process.env.PORT || 1337;
+
+fbAdmin.initializeApp({
+	credential: admin.credential.cert(serviceAccount);
+});
+
+let db = admin.firestore();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist'));

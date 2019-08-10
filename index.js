bea16ed881ee08e611358fc1f5657b9aef37ec8a
Matt <Matt@Homes-MacBook-Air.local>
2019-08-10 08:12:35 -0700

    Update db data model for firestore

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fbAdmin = require('firebase-admin');
-
-let serviceAccount = require('./secrets/shmc.json');
+const db = require('./db/db');
 
 const app = express();
 const port = process.env.PORT || 1337;
 
-fbAdmin.initializeApp({
-	credential: fbAdmin.credential.cert(serviceAccount),
-	databaseURL: "https://shmc-9f46a.firebaseio.com"
-});
-
-let db = fbAdmin.firestore();
-
-let docRef = db.collection('handicaps').doc('1');
-
-let setAda = docRef.set({
-  Handicap: 1,
-  Black: 2,
-  White: 2
-});
-
-db.collection('handicaps').get()
+db.db.collection('handicaps').get()
 	.then((snapshot) => {
 	  snapshot.forEach((doc) => {
 	    console.log(doc.id, '=>', doc.data());

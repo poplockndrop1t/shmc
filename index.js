5653c5c31f1fb4212c19b7513cd0e4dc2a386f85
woonandez <matthew.allen.fernandez@gmail.com>
2019-08-10 08:31:10 -0700

    Server now handles get requests and responds with firebase data

--- index.js
+++ index.js
@@ -5,19 +5,18 @@ const db = require('./db/db');
 const app = express();
 const port = process.env.PORT || 1337;
 
-db.db.collection('handicaps').get()
-	.then((snapshot) => {
-	  snapshot.forEach((doc) => {
-	    console.log(doc.id, '=>', doc.data());
-	  });
-	})
-	.catch((err) => {
-	  console.log('Error getting documents', err);
-	});
-
-
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/handicaps/all', (req, res) => {
+	db.db.collection('handicaps').get()
+		.then((snapshot) => {
+			snapshot.forEach((doc) => {
+				res.send(doc.data());
+			});
+		})
+		.catch(err => console.log('Error', err));
+})
 
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/dist/index.html');

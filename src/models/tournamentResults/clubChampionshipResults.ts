bee965ab950fd7574a1fef330d781b57451616db
woonandez <matthew.allen.fernandez@gmail.com>
2019-09-03 16:47:25 -0700

    Update the club championship results

--- src/models/tournamentResults/clubChampionshipResults.ts
+++ src/models/tournamentResults/clubChampionshipResults.ts
@@ -3,37 +3,37 @@ export class ClubChampionshipData {
     {
       flight: "Championships",
       player: "Mike Leibin",
-      points: 129,
+      points: 222,
       place: "1st"
     },
     {
       flight: "Championships",
       player: "Dylan Gaughen",
-      points: 120,
+      points: 225,
       place: "2nd"
     },
     {
       flight: "Championships",
-      player: "Pat Belivick",
-      points: 134,
+      player: "Patrick Belavic",
+      points: 227,
       place: "3rd"
     },
     {
       flight: "Sr. Club Championships",
-      player: "Brody",
-      points: 120,
+      player: "Brody Munro",
+      points: 154,
       place: "1st",
     },
     {
       flight: "Sr. Club Championships",
       player: "Greg Hatfield",
-      points: 140,
+      points: 161,
       place: "2nd"
     },
     {
       flight: "Sr. Club Championships",
       player: "Eddie Lindgren",
-      points: 145,
+      points: 164,
       place: "3rd"
     },
     {
@@ -45,8 +45,38 @@ export class ClubChampionshipData {
     {
       flight: "Net Club Championship",
       player: "Terry Powell",
-      points: 145,
+      points: 140,
       place: "1st"
+    },
+    {
+      flight: "Net Club Championship",
+      player: "Greg Hatfield",
+      points: 153,
+      place: "2nd"
+    },
+    {
+      flight: "Net Club Championship Flight B",
+      player: "Mark Sallin",
+      points: 150,
+      place: "1st"
+    },
+    {
+      flight: "Net Club Championship Flight B",
+      player: "Michael Jankowski",
+      points: 150,
+      place: "2nd"
+    },
+    {
+      flight: "Net Club Championship Flight C",
+      player: "Warren Hinka",
+      points: 149,
+      place: "1st"
+    },
+    {
+      flight: "Net Club Championship Flight C",
+      player: "Frank Sherman",
+      points: 156,
+      place: "2nd"
     }
   ];
 }

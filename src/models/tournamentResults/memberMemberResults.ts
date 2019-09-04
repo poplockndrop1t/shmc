b38b8b3c171eda477857b300e6f3339cf103dc03
woonandez <matthew.allen.fernandez@gmail.com>
2019-09-03 17:05:21 -0700

    Add results data for member member

---
+++ src/models/tournamentResults/memberMemberResults.ts
@@ -0,0 +1,82 @@
+export class MemberGuestData {
+  memberGuest2019 = [
+    {
+      flight: "Member Member Club Champions",
+      player: "Mike Brennen, and Kevin O'Toole",
+      points: 199,
+      place: "Champions"
+    },
+    {
+      flight: "A",
+      player: "Dylan Gaughen, and Rob Jacobus",
+      points: 204,
+      place: "Flight A Gross Champions"
+    },
+    {
+      flight: "A",
+      player: "Rick Cromwell, and Brodie Munro",
+      points: 207,
+      place: "Flight A 2nd Place Gross"
+    },
+    {
+      flight: "A",
+      player: "John Billin, and Jeff Halloran",
+      points: 205,
+      place: "Flight A Net Champions"
+    },
+    {
+      flight: "A",
+      player: "Daniel Goldfield, and Drew Saberhagen",
+      points: 207,
+      place: "Flight A 2nd Place Net"
+    },
+    {
+      flight: "B",
+      player: "Steve Alexander, and Jim Pett",
+      points: 201
+      place: "Flight B Gross Champions"
+    },
+    {
+      flight: "B",
+      player: "Peter Eyermann, and Ron Fernandez",
+      points: 212,
+      place: "Flight B 2nd Place Gross"
+    },
+    {
+      flight: "B",
+      player: "Mike Brennen, and Kevin O'Toole",
+      points: 199,
+      place: "Flight B Net Champions"
+    },
+    {
+      flight: "B",
+      player: "Alex Lopez, and Tim Neufeld",
+      points: 208,
+      place: "Flight B 2nd Place Net"
+    },
+    {
+      flight: "C",
+      player: "Marc Neale, and Rick Ord",
+      points: 222,
+      place: "Flight C Gross Champions"
+    },
+    {
+      flight: "C",
+      player: "Cliff Hodge, and Mike Treuheit",
+      points: 222,
+      place: "Flight C 2nd Place Gross"
+    },
+    {
+      flight: "C",
+      player: "Fred Robertson, and chris Weinstock",
+      points: 215,
+      place: "Flight C Net Champions"
+    },
+    {
+      flight: "C",
+      player: "Ed Marsango, and Bob Scott",
+      points: 218,
+      place: "Flight C 2nd Place Net"
+    }
+  ];
+}

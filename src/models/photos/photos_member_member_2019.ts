84176b8c641d80d8f95a779ec2975b0046a5d68b
woonandez <matthew.allen.fernandez@gmail.com>
2019-09-06 08:50:23 -0700

    Add member member photos

---
+++ src/models/photos/photos_member_member_2019.ts
@@ -0,0 +1,39 @@
+export class Member_Member_Photos {
+  photos = [
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_1.jpeg',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_2.jpeg',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_3.jpeg',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_4.jpeg',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_5.jpeg',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_6.jpeg',
+      desc: ''
+    },
+    {
+      name: '',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_7.jpeg',
+      desc: ''
+    }
+  ];
+}

f019f5b9c9abfb3b63042a73c6aa79f2b4d0905c
woonandez <matthew.allen.fernandez@gmail.com>
2019-09-06 08:57:06 -0700

    Member member photos are now imported as JPG

--- src/models/photos/photos_member_member_2019.ts
+++ src/models/photos/photos_member_member_2019.ts
@@ -2,37 +2,37 @@ export class Member_Member_Photos {
   photos = [
     {
       name: '',
-      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_1.jpeg',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_1.JPG',
       desc: ''
     },
     {
       name: '',
-      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_2.jpeg',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_2.JPG',
       desc: ''
     },
     {
       name: '',
-      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_3.jpeg',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_3.JPG',
       desc: ''
     },
     {
       name: '',
-      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_4.jpeg',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_4.JPG',
       desc: ''
     },
     {
       name: '',
-      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_5.jpeg',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_5.JPG',
       desc: ''
     },
     {
       name: '',
-      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_6.jpeg',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_6.JPG',
       desc: ''
     },
     {
       name: '',
-      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_7.jpeg',
+      src: 'assets/img/shmc_pics/2019_member_member_pics/IMG_7.JPG',
       desc: ''
     }
   ];

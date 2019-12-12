55fa4ee7debb5ca4efb3015de82213924a59f4a6
woonandez <matthew.allen.fernandez@gmail.com>
2019-12-12 12:15:37 -0800

    Add form validation on submit

--- src/app/components/email/email.component.ts
+++ src/app/components/email/email.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-email',
@@ -8,17 +8,29 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class EmailComponent implements OnInit {
   profileForm = new FormGroup({
-    header: new FormControl(''),
-    name: new FormControl(''),
-    email: new FormControl(''),
-    message: new FormControl('')
+    header: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required)
   });
 
   constructor() {}
 
   ngOnInit() {}
 
+  // onHover() {
+  //   let header = this.profileForm.value.header;
+  //   let name = this.profileForm.value.name;
+  //   let email = this.profileForm.value.email;
+  //   let message = this.profileForm.value.message;
+  //   console.log(header, name, email, message);
+  //   if (header.length > 0 && name.length > 0 && email.length > 0 && message.length > 0) {
+  //     this.profileForm.valid = !this.profileForm.valid;
+  //   }
+  // }
+
   onSubmit() {
     console.log('ran', this.profileForm.value);
+    console.log('valid', this.profileForm.valid);
   }
 }
